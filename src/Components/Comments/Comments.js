c8448cfd296bad842745e0f4675a499c5395a06c
stanislav.bigunenko <stas.bigunenko@gmail.com>
2022-01-17 16:12:32 +0200

    Changed Comments functionality

--- src/Components/Comments/Comments.js
+++ src/Components/Comments/Comments.js
@@ -1,88 +1,20 @@
 import React, {useEffect, useState} from 'react';
-import MyModal from "../MyModal/MyModal.js";
-import DishesService from "../../API/DischesService.js";
-import Comment from "../Comment/Comment.js";
 import classes from "./Comments.module.css"
+import {useHistory} from "react-router-dom";
+
 
 const Comments = ({dishID}) => {
 
-    // Hook for controlling the state of the receipts
-    const [comments, setComments] = useState([])
-    //Hook for controlling the state of the comment body
-    const [commentBody, setCommentBody] = useState('')
-    //Hook for controlling the state of the author comment
-    const [commentAuthor, setCommentAuthor] = useState('')
+    const router = useHistory()
 
-    // Function to receive all comments from the db
-    async function fetchComments() {
-        const comments = await DishesService.getAllComments(dishID)
-        setComments(comments)
-    }
-
-    async function createComment(e) {
-        e.preventDefault()
-        const newComments = await DishesService.createComment(commentAuthor, commentBody, comments, dishID)
-        setComments(newComments)
-    }
-
-    const [modalActive, setModalActive] = useState(false)
     return (
-        <div>
         <button
             className={classes.comments__btn}
             onClick={() => {
-            fetchComments()
-            setModalActive(true)
+                router.push(`/recipes/${dishID}`)
         }} >
             Комментарии
         </button>
-        <MyModal active={modalActive} setActive={setModalActive}>
-            <form
-                onSubmit={e => {
-                createComment(e)
-                setModalActive(false)
-                setCommentBody('')
-                setCommentAuthor('')
-            }}>
-                <input
-                    style={{marginBottom:"12px", justifySelf:"center", margin:"12px"}}
-                    placeholder='Ваше имя'
-                    value={commentAuthor}
-                    onChange={e => setCommentAuthor(e.target.value)}
-                    required={true}
-                />
-                <textarea
-                    style={{width:"670px", height:"100px"}}
-                    placeholder='Оставьте свой комментарий здесь...'
-                    value={commentBody}
-                    onChange={e => setCommentBody(e.target.value)}
-                    required={true}
-                />
-                <input
-                    className={classes.comments__btn2}
-                    type="submit"
-                    value="Добавить"
-                />
-                    <button
-                        className={classes.comments__btn3}
-                        onClick={(e) => {
-                            e.preventDefault()
-                            setModalActive(false)
-                            setCommentBody('')
-                        }}
-                    > Отменить </button>
-            </form>
-            <h3 style={{textAlign:'center', marginTop:'15px'}}>
-                Комментарии к рецепту
-            </h3>
-            {comments.map(comment =>
-            <Comment
-                comment={comment}
-                key={dishID + Math.random()}
-            />
-            )}
-        </MyModal>
-        </div>
     );
 };
 
